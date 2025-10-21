73a7d088944461a55a9c68c64fc1ba710aa0a1f3
David Cirenese <davidcirenese@yahoo.com>
2025-10-20 22:27:04 -0400

    update BASE_URL to point to the production server

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "http://yoursay-16cb.onrender.com:8080";
 
 export async function signupUser(data) {
   const res = await fetch(`${BASE_URL}/api/users`, {
